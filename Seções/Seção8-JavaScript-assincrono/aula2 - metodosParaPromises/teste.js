264c6275bfe5bad15651733e02fae85723db6025
Walther Oliveira Pires <102191627+waltherpires@users.noreply.github.com>
2023-09-27 04:34:16 -0300

    aula5 - Fecth API

--- Seções/Seção8-JavaScript-assincrono/aula2 - metodosParaPromises/teste.js
+++ Seções/Seção8-JavaScript-assincrono/aula2 - metodosParaPromises/teste.js
@@ -1,8 +1,7 @@
-function rand(min, max){
+function rand(min = 0, max =3){
     min *= 1000;
     max *= 1000;
     return Math.floor(Math.random() * (max-min) + min)
-
 }
 
 function esperaAi(msg, tempo) {
@@ -14,8 +13,42 @@ function esperaAi(msg, tempo) {
             }
 
             resolve(msg.toUpperCase() + ' - Passei na promise');
+            return;
         }, tempo);
     });  
 }
 
+// esperaAi('Fase 1', rand());
+//     .then(valor => {
+//         console.log(valor);
+//         return esperaAi('Fase 2', rand());
+//     })
+//     .then(fase => {
+//         console.log(fase);
+//         return esperaAi('Fase 3', rand());
+//     })
+//     .then(fase => {
+//         console.log(fase);
+//         return fase;
+//     })
+//     .then(fase => {
+//         console.log('Terminanos na fase: ', fase);
+//     })
+//     .catch(e => console.log(e));
 
+async function executa() {
+    try {
+        const fase1 = await esperaAi('Fase 1', rand());
+        console.log(fase1);
+     
+        const fase2 = await esperaAi('Fase 2', rand());
+        console.log(fase2);
+        
+        const fase3 = await esperaAi('Fase 3', rand());
+         console.log(fase3);
+     
+        console.log('Terminamos na fase: ', fase3);
+    } catch(e){
+        console.log(e);
+    }
+}
---
+++ Seções/Seção8-JavaScript-assincrono/aula3 - asyncEawait/index.js
@@ -0,0 +1,39 @@
+function rand(min, max){
+    min *= 1000;
+    max *= 1000;
+    return Math.floor(Math.random() * (max-min) + min)
+
+}
+
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof msg !== 'string') {
+                reject('CAI NO ERRO');
+                return;
+            }
+
+            resolve(msg.toUpperCase() + ' - Passei na promise');
+        }, tempo);
+    });  
+}
+
+async function executa() {
+    try {
+        const fase1 = await esperaAi('Fase 1', rand());
+        console.log(fase1);
+     
+        const fase2 = await esperaAi('Fase 2', rand());
+        console.log(fase2);
+        
+        const fase3 = await esperaAi('Fase 3', rand());
+         console.log(fase3);
+     
+        console.log('Terminamos na fase: ', fase3);
+    } catch(e){
+        console.log(e);
+    }
+}
+
+executa();
+    
