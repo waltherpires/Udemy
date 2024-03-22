ab22eb5b5254070cdb1642b15921b11c11bb3c90
Walther <waltherpires@hotmail.com>
2024-03-22 03:31:24 -0300

    Adicionando cors

--- Secoes/api_rest/app.js
+++ Secoes/api_rest/app.js
@@ -5,12 +5,29 @@ dotenv.config();
 
 import './src/database';
 
+import cors from 'cors';
+import helmet from 'helmet';
+
 import express from 'express';
 import homeRoutes from './src/routes/homeRoutes';
 import fotoRoutes from './src/routes/fotoRoutes';
 import userRoutes from './src/routes/userRoutes';
 import tokenRoutes from './src/routes/tokenRoutes';
 import alunoRoutes from './src/routes/alunoRoutes';
+
+const whiteList = [
+  'https://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -20,6 +37,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extend: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
--- Secoes/api_rest/src/controllers/TokenController.js
+++ Secoes/api_rest/src/controllers/TokenController.js
@@ -24,12 +24,13 @@ class TokenController {
         errors: ['Senha inválida'],
       });
     }
+
     const { id } = user;
     const token = jwt.sign({ id, email }, process.env.TOKEN_SECRET, {
       expiresIn: process.env.TOKEN_EXPIRATION,
     });
 
-    return res.json({ token });
+    return res.json({ token, user: { nome: user.nome, id, email } });
   }
 }
 
