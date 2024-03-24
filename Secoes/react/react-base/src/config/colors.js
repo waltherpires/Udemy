d4a3f5bd17b4c78eda7ab398d6b58f80ae33a7b4
Walther <waltherpires@hotmail.com>
2024-03-24 09:58:43 -0300

    reactbase

--- Secoes/react/react-base/src/config/colors.js
+++
@@ -1,7 +0,0 @@
-export const primaryColor = '#000000';
-export const primaryDarkColor = '#1A1A1D';
-
-export const sucessColor = '#O197f6';
-export const infoColor = '#O197f6';
-export const errorColor = '#F2AF29';
-export const warningCOlor = '#F2AF29';
--- Secoes/react/reactbase/.eslintrc.js
+++ Secoes/react/reactbase/.eslintrc.js
@@ -4,9 +4,13 @@ module.exports = {
     es2021: true,
   },
   extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
     'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'airbnb/hooks',
     'airbnb',
-    'plugin:prettier/recommended',
+    'prettier',
   ],
   overrides: [
     {
@@ -21,23 +25,31 @@ module.exports = {
   ],
   parser: '@babel/eslint-parser',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    requireConfigFile: false,
     babelOptions: {
       presets: ['@babel/preset-react'],
     },
-    ecmaVersion: 'latest',
-    requireConfigFile: false,
-    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-  plugins: ['prettier', 'react', 'react-hooks'],
   rules: {
+    'react/prop-types': 0,
+    'no-console': 1,
     'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-uses-react': 0,
     'react/jsx-filename-extension': 0,
     'import/prefer-default-export': 0,
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-    'jsx-a11y/control-has-associated-label': 0,
+  },
+  plugins: ['prettier', 'react-hooks', 'react', 'import', 'jsx-a11y'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
